5a01749e05018409a4c9e2ac4f708aaccdd6b027
Climent Olives <mente.olives@gmail.com>
2022-03-29 14:20:21 +0200

    feina

--- src/components/_layout/NavbarPrivate.vue
+++ src/components/_layout/NavbarPrivate.vue
@@ -1,11 +1,38 @@
 <template>
-<nav>
+  <div>
+    <nav>
       <router-link to="/proposals">Propuestas</router-link> | 
       <router-link to="/tasks">Tareas</router-link> | 
       <router-link to="/logout">Salir</router-link> | 
       
     </nav>
+    <b-button v-b-toggle.sidebar-1>Toggle Sidebar</b-button>
+    <b-sidebar id="sidebar-1" v-b-visible.350="visibleHandler" title="Sidebar" shadow>
+      <div class="px-3 py-2">
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+          in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+        </p>
+        <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+      </div>
+    </b-sidebar>
+  </div>
+
+
+    
     </template>
 <script>
-
+export default {
+  methods: {
+    visibleHandler(isVisible) {
+      if (isVisible) {
+        alert("x");
+        // Do something
+      } else {
+        alert("z")
+        // Do something else
+      }
+    }
+  }
+}
 </script>
